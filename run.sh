994e75fc267e97be20ea134019c564ac4c370aae
josemarcellolopes <jm.lopes@live.com>
2025-03-09 21:10:29 -0300

    v1 refatorada

--- run.sh
+++ run.sh
@@ -14,9 +14,11 @@ echo "3) Airflow"
 echo "4) N8n"
 echo "5) Delta Lake"
 echo "6) MinIO (S3)"
-echo "7) Todas Stacks"
 echo ""
-echo "0) Sair e/ou baixar todas as stacks"
+echo "a) Ativar todas stacks"
+echo "d) Desativar stacks ativas"
+echo ""
+echo "0) Sair"
 echo ""
 
 read -p "Digite as opções desejadas separadas por espaço (ex: 1 3): " input
@@ -27,37 +29,39 @@ for option in $input; do
     case $option in
         1) echo "Iniciando Briefer..."; 
                 echo ""
-                docker-compose -f docker/docker-compose-briefer.yml up -d ;;
+                docker-compose -f docker/docker-compose-briefer.yml up -d 2>&1 | grep -v "WARN";;
 
         2) echo "Iniciando MySQL..."; 
                 echo ""
-                docker-compose -f docker-compose-mysql.yml up -d ;;
+                docker-compose -f docker/docker-compose-mysql.yml up -d 2>&1 | grep -v "WARN";;
 
         3) echo "Iniciando Airflow..."; 
                 echo ""
-                docker-compose -f docker/docker-compose-airflow.yml up -d ;;
+                docker-compose -f docker/docker-compose-airflow.yml up -d 2>&1 | grep -v "WARN";;
 
         4) echo "Iniciando N8n..."; 
                 echo ""
-                docker-compose -f docker/docker-compose-n8n.yml up -d ;;
+                docker-compose -f docker/docker-compose-n8n.yml up -d 2>&1 | grep -v "WARN";;
 
         5) echo "Iniciando Delta Lake..."; 
                 echo ""
-                docker-compose -f docker/docker-compose-deltaio.yml up -d ;;
+                docker-compose -f docker/docker-compose-deltaio.yml up -d 2>&1 | grep -v "WARN";;
 
         6) echo "Iniciando MinIO (S3)..."; 
                 echo ""
-                docker-compose -f docker/docker-compose-minio.yml up -d ;;
+                docker-compose -f docker/docker-compose-minio.yml up -d 2>&1 | grep -v "WARN";;
 
-        7) echo "Iniciando todas as stacks...";
+        a) echo "Ativando todas as stacks...";
                 echo ""
-                docker-compose -f docker/docker-compose-briefer.yml -f docker/docker-compose-mysql.yml -f docker/docker-compose-airflow.yml -f docker/docker-compose-n8n.yml -f docker/docker-compose-deltaio.yml -f docker/docker-compose-minio.yml up -d ;;
+                docker-compose -f docker/docker-compose-briefer.yml -f docker/docker-compose-mysql.yml -f docker/docker-compose-airflow.yml -f docker/docker-compose-n8n.yml -f docker/docker-compose-deltaio.yml -f docker/docker-compose-minio.yml up -d 2>&1 | grep -v "WARN";;
+
+        d) echo "Desativando todas as stacks...";
+                echo ""
+                docker stop $(docker ps -aq)
+                docker rm $(docker ps -aq)
+                docker network prune -f;;
 
         0) echo "Saindo..."; 
-                echo ""
-                cd docker
-                docker-compose down
-                cd ..
                 exit 0;;
 
         *) echo "Opção inválida: $option";;
