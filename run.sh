3679f133c1687aed00744895faa4982f43845b0c
josemarcellolopes <jm.lopes@live.com>
2025-03-10 07:55:37 -0300

    Melhoria README.md

--- run.sh
+++ run.sh
@@ -15,6 +15,8 @@ echo "4) N8n"
 echo "5) Delta Lake"
 echo "6) MinIO (S3)"
 echo ""
+echo "7) Obter token para execução Jupyter Notebook"
+echo ""
 echo "a) Ativar todas stacks"
 echo "d) Desativar stacks ativas"
 echo ""
@@ -29,41 +31,67 @@ for option in $input; do
     case $option in
         1) echo "Iniciando Briefer..."; 
                 echo ""
-                docker-compose -f docker/docker-compose-briefer.yml up -d 2>&1 | grep -v "WARN";;
+                docker-compose -f docker/docker-compose-briefer.yml up -d 2>&1 | grep -v "WARN"
+                echo ""
+                exit 0;;
 
         2) echo "Iniciando MySQL..."; 
                 echo ""
-                docker-compose -f docker/docker-compose-mysql.yml up -d 2>&1 | grep -v "WARN";;
+                docker-compose -f docker/docker-compose-mysql.yml up -d 2>&1 | grep -v "WARN"
+                echo ""
+                exit 0;;
 
         3) echo "Iniciando Airflow..."; 
                 echo ""
-                docker-compose -f docker/docker-compose-airflow.yml up -d 2>&1 | grep -v "WARN";;
+                docker-compose -f docker/docker-compose-airflow.yml up -d 2>&1 | grep -v "WARN"
+                echo ""
+                exit 0;;
 
         4) echo "Iniciando N8n..."; 
                 echo ""
-                docker-compose -f docker/docker-compose-n8n.yml up -d 2>&1 | grep -v "WARN";;
+                docker-compose -f docker/docker-compose-n8n.yml up -d 2>&1 | grep -v "WARN"
+                echo ""
+                exit 0;;
 
         5) echo "Iniciando Delta Lake..."; 
                 echo ""
-                docker-compose -f docker/docker-compose-deltaio.yml up -d 2>&1 | grep -v "WARN";;
+                docker-compose -f docker/docker-compose-deltaio.yml up -d 2>&1 | grep -v "WARN"
+                echo ""
+                exit 0;;
 
         6) echo "Iniciando MinIO (S3)..."; 
                 echo ""
-                docker-compose -f docker/docker-compose-minio.yml up -d 2>&1 | grep -v "WARN";;
+                docker-compose -f docker/docker-compose-minio.yml up -d 2>&1 | grep -v "WARN"
+                echo ""
+                exit 0;;
+
+        7) echo "Token..."; 
+                echo ""
+                docker exec -it delta_lake jupyter server list
+                echo ""
+                exit 0;;
 
         a) echo "Ativando todas as stacks...";
                 echo ""
-                docker-compose -f docker/docker-compose-briefer.yml -f docker/docker-compose-mysql.yml -f docker/docker-compose-airflow.yml -f docker/docker-compose-n8n.yml -f docker/docker-compose-deltaio.yml -f docker/docker-compose-minio.yml up -d 2>&1 | grep -v "WARN";;
+                docker-compose -f docker/docker-compose-briefer.yml -f docker/docker-compose-mysql.yml -f docker/docker-compose-airflow.yml -f docker/docker-compose-n8n.yml -f docker/docker-compose-deltaio.yml -f docker/docker-compose-minio.yml up -d 2>&1 | grep -v "WARN"
+                echo ""
+                exit 0;;
 
         d) echo "Desativando todas as stacks...";
                 echo ""
                 docker stop $(docker ps -aq)
                 docker rm $(docker ps -aq)
-                docker network prune -f;;
-
-        0) echo "Saindo..."; 
+                docker network prune -f
+                echo ""
                 exit 0;;
 
-        *) echo "Opção inválida: $option";;
+        0) echo "Saindo..."; 
+                echo ""
+                exit 0;;
+
+        *) echo "Opção inválida: $option";
+                echo ""
+                exit 0;;
+
     esac
 done
